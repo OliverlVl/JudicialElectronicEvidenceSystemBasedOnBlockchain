84b82fb6befede3786eb03b060a2a832ee190d49
OliverlVl <54763262+OliverlVl@users.noreply.github.com>
2021-12-09 23:45:06 +0800

    添加序列号

--- vue2-manage/src/App.vue
+++ vue2-manage/src/App.vue
@@ -1,15 +1,22 @@
 <template>
-    <div id="app" class="fillcontain">
-		    <router-view></router-view>
-    </div>
+  <div id="app" class="fillcontain">
+    <router-view></router-view>
+    <div class="version" v-if="this.$route.path != '/'">序列号：YC-21-03</div>
+  </div>
 </template>
 
 <script>
-    export default {
-    	
-    }
+export default {};
 </script>
 
 <style lang="less">
-	@import './style/common';
+@import "./style/common";
+
+.version {
+  position: fixed;
+  right: 10px;
+  bottom: 20px;
+  font-style: italic;
+  font-weight: bold;
+}
 </style>
